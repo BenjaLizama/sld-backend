1205410d7311f4c14b2e4ed1427bd23101bf7781
Benjamin Lizama <be.lizamac@duocuc.cl>
2026-04-15 21:20:03 -0400

    feat: Configuración de seguridad basica para la aplicación

---
+++ autenticacion-seguridad/src/main/java/com/promptlabs/autenticacion_seguridad/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.promptlabs.autenticacion_seguridad.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // CSRF deshabilitado para utilizar el cliente H2 y POSTMAN sin problemas.
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2/console/**").permitAll() // TODO: Solo un administrador debería tener acceso a esto.
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                // Permite que la consola de H2 se cargue en un frame.
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
