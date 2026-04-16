bc6b15ea5b34042ed956e410b421f7f30a953d23
Benjamin Lizama <be.lizamac@duocuc.cl>
2026-04-15 21:59:30 -0400

    fix: Error con seguridad, posible problema con .formLogin e implementa bean AuthenticationManager

--- autenticacion-seguridad/src/main/java/com/promptlabs/autenticacion_seguridad/config/SecurityConfig.java
+++ autenticacion-seguridad/src/main/java/com/promptlabs/autenticacion_seguridad/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.promptlabs.autenticacion_seguridad.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,18 +23,22 @@ public class SecurityConfig {
                 // CSRF deshabilitado para utilizar el cliente H2 y POSTMAN sin problemas.
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2/console/**").permitAll() // TODO: Solo un administrador debería tener acceso a esto.
+                        .requestMatchers("/h2/h2-console/**").permitAll() // TODO: Solo un administrador debería tener acceso a esto.
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated())
                 // Permite que la consola de H2 se cargue en un frame.
-                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-                .formLogin(Customizer.withDefaults());
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
         return httpSecurity.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
