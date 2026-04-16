d6987181ef505f61484d9ab295460521672fa64b
Benjamin Lizama <be.lizamac@duocuc.cl>
2026-04-15 23:39:27 -0400

    feat: Entidad Privilegios

---
+++ autenticacion-seguridad/src/main/java/com/promptlabs/autenticacion_seguridad/entity/PrivilegeEntity.java
@@ -0,0 +1,24 @@
+package com.promptlabs.autenticacion_seguridad.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Table(name = "PRIVILEGE")
+public class PrivilegeEntity extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String name; // Ej: READ_USERS, WRITE_USERS.
+
+    @Column(nullable = false)
+    private String description;
+
+}
