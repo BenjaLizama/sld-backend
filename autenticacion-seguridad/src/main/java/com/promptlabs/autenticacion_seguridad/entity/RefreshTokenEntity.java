37956a0a32cf99f4f224abc6e1b3043bc0bc69a4
Benjamin Lizama <be.lizamac@duocuc.cl>
2026-04-15 23:55:28 -0400

    feat: Entidad RefreshToken

---
+++ autenticacion-seguridad/src/main/java/com/promptlabs/autenticacion_seguridad/entity/RefreshTokenEntity.java
@@ -0,0 +1,26 @@
+package com.promptlabs.autenticacion_seguridad.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Table(name = "REFRESH_TOKEN")
+public class RefreshTokenEntity extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+    @OneToOne
+    @JoinColumn(name = "credential_id", referencedColumnName = "id")
+    private CredentialEntity credential;
+
+}
