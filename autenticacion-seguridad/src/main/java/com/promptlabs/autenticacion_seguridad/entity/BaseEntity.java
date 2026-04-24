d2f1b6704d31d3eea9076efefd6a554e5ace7b04
Benjamin Lizama <be.lizamac@duocuc.cl>
2026-04-23 20:10:48 -0400

    refactor: Ahora utiliza @CreationTimestamp en fecha de creación y actualización.

--- autenticacion-seguridad/src/main/java/com/promptlabs/autenticacion_seguridad/entity/BaseEntity.java
+++ autenticacion-seguridad/src/main/java/com/promptlabs/autenticacion_seguridad/entity/BaseEntity.java
@@ -5,8 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
@@ -24,11 +23,11 @@ public abstract class BaseEntity {
     @Column(updatable = false, nullable = false)
     private UUID id;
 
-    @CreatedDate
+    @CreationTimestamp
     @Column(updatable = false)
     private Instant createdAt;
 
-    @LastModifiedDate
+    @CreationTimestamp
     private Instant updatedAt;
 
     @Column(name = "is_active")
