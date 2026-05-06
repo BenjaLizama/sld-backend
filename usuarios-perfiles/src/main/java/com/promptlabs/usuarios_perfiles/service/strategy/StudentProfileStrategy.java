69ffc8183472aacf990d9f83ff0be4f69798126c
Lucciano <zorrapeluahihu@gmail.com>
2026-05-06 01:54:21 -0400

    feat: Agrega creacion de StudentProfile

---
+++ usuarios-perfiles/src/main/java/com/promptlabs/usuarios_perfiles/service/strategy/StudentProfileStrategy.java
@@ -0,0 +1,24 @@
+package com.promptlabs.usuarios_perfiles.service.strategy;
+
+import com.promptlabs.usuarios_perfiles.entity.StudentProfile;
+import com.promptlabs.usuarios_perfiles.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentProfileStrategy implements ProfileCreationStrategy {
+
+
+    @Override
+    public String getRole() {
+        return "STUDENT";
+    }
+
+    @Override
+    public void createEmptyProfile(User user){
+        StudentProfile profile = StudentProfile.builder().user(user).build();
+
+        user.setStudentProfile(profile);
+
+
+    }
+}
