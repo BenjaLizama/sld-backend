a41becde15fa5373675db3bbde0120f6f8a866c5
Lucciano <zorrapeluahihu@gmail.com>
2026-05-06 01:41:14 -0400

    config: Agrega datos por defecto en ParentType y Gender

---
+++ usuarios-perfiles/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO gender (id, gender) VALUES (1, 'MASCULINO');
+INSERT INTO gender (id, gender) VALUES (2, 'FEMENINO');
+INSERT INTO gender (id, gender) VALUES (3, 'OTRO');
+
+INSERT INTO parent_type (relationship) VALUES ('Madre');
+INSERT INTO parent_type (relationship) VALUES ('Padre');
+INSERT INTO parent_type (relationship) VALUES ('Abuelo/a');
+INSERT INTO parent_type (relationship) VALUES ('Tío/a');
+INSERT INTO parent_type (relationship) VALUES ('Hermano/a Mayor');
+INSERT INTO parent_type (relationship) VALUES ('Tutor Legal');
