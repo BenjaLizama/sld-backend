109679d7e9f62d235cdec6c434c382ce9e6f9e95
Lucciano <zorrapeluahihu@gmail.com>
2026-05-11 20:51:05 -0400

    primer commit

---
+++ backend-for-frontend/src/test/java/com/promptlabs/backend_for_frontend/BackendForFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promptlabs.backend_for_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendForFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
