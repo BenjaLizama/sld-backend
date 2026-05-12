8d869ea1fe07b41a40fed3f7dee041f760723336
Lucciano <zorrapeluahihu@gmail.com>
2026-05-12 15:24:44 -0400

    fix: correccion de RabbitMQConfig

--- autenticacion-seguridad/src/main/java/com/promptlabs/autenticacion_seguridad/config/RabbitMQConfig.java
+++ autenticacion-seguridad/src/main/java/com/promptlabs/autenticacion_seguridad/config/RabbitMQConfig.java
@@ -1,41 +1,32 @@
 package com.promptlabs.autenticacion_seguridad.config;
 
-
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 
 @Configuration
 public class RabbitMQConfig {
 
     public static final String EXCHANGE = "user_exchange";
-    public static final String QUEUE = "user_creation_queue";
     public static final String ROUTING_KEY = "user_routing_key";
 
     @Bean
-    public Queue queue() { return new Queue(QUEUE); }
-
-    @Bean
-    public DirectExchange exchange() { return new DirectExchange(EXCHANGE); }
-
-    @Bean
-    public Binding binding(Queue queue, DirectExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE);
     }
+
     @Bean
-    public JacksonJsonMessageConverter messageConverter() {
+    public MessageConverter messageConverter() {
         return new JacksonJsonMessageConverter();
     }
 
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
-                                         JacksonJsonMessageConverter messageConverter) {
+                                         MessageConverter messageConverter) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
