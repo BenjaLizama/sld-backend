572b4e68823a3286b5e4431a76058756e5e5957e
lucciano martinez <zorrapeluahihu@gmail.com>
2026-06-11 17:30:54 -0400

    feat: agrega metodo para buscar relacion de perentescos mediante sus partisipantes

--- usuarios-perfiles/src/main/java/com/promptlabs/usuarios_perfiles/repository/FamilyRelationshipRepository.java
+++ usuarios-perfiles/src/main/java/com/promptlabs/usuarios_perfiles/repository/FamilyRelationshipRepository.java
@@ -1,7 +1,11 @@
 package com.promptlabs.usuarios_perfiles.repository;
 
 import com.promptlabs.usuarios_perfiles.entity.FamilyRelationship;
+import com.promptlabs.usuarios_perfiles.entity.ParentProfile;
+import com.promptlabs.usuarios_perfiles.entity.StudentProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FamilyRelationshipRepository extends JpaRepository<FamilyRelationship,Long> {
+    boolean existsByParentProfileAndStudentProfile(ParentProfile parent, StudentProfile student);
 }
+
