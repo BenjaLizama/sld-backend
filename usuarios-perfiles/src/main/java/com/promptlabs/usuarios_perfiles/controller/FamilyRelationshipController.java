de79f06d39130d031661dbb83ff213f4acf94993
lucciano martinez <zorrapeluahihu@gmail.com>
2026-06-11 17:34:03 -0400

    refactor: modifica endpoint de crear parentesco para retornar una respuesta y no solo texto

--- usuarios-perfiles/src/main/java/com/promptlabs/usuarios_perfiles/controller/FamilyRelationshipController.java
+++ usuarios-perfiles/src/main/java/com/promptlabs/usuarios_perfiles/controller/FamilyRelationshipController.java
@@ -1,9 +1,11 @@
 package com.promptlabs.usuarios_perfiles.controller;
 
 import com.promptlabs.usuarios_perfiles.dto.LinkFamilyRequest;
+import com.promptlabs.usuarios_perfiles.dto.LinkFamilyResponse;
 import com.promptlabs.usuarios_perfiles.service.FamilyRelationshipService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,10 +15,9 @@ import org.springframework.web.bind.annotation.*;
 public class FamilyRelationshipController {
 
     private final FamilyRelationshipService familyRelationshipService;
-
     @PostMapping("/link")
-    public ResponseEntity<String> linkFamily(@Valid @RequestBody LinkFamilyRequest request) {
-        familyRelationshipService.linkFamily(request);
-        return ResponseEntity.ok("Vínculo familiar creado con éxito");
+    public ResponseEntity<LinkFamilyResponse> linkFamily(@Valid @RequestBody LinkFamilyRequest request) {
+        LinkFamilyResponse response = familyRelationshipService.linkFamily(request);
+        return new ResponseEntity<>(response,HttpStatus.CREATED);
     }
 }
