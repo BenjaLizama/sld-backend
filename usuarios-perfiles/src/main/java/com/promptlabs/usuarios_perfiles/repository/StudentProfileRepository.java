7ac9e1d0a5e2a01f8c57ce1b064f94a435e05a8b
lucciano martinez <zorrapeluahihu@gmail.com>
2026-06-11 17:02:53 -0400

    feat: agrega metodo para obtener el id de cada perfin mediante el rut

--- usuarios-perfiles/src/main/java/com/promptlabs/usuarios_perfiles/repository/StudentProfileRepository.java
+++ usuarios-perfiles/src/main/java/com/promptlabs/usuarios_perfiles/repository/StudentProfileRepository.java
@@ -4,8 +4,10 @@ import com.promptlabs.usuarios_perfiles.entity.StudentProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface StudentProfileRepository extends JpaRepository<StudentProfile, UUID> {
+    Optional<StudentProfile> findByUserRut(String rut);
 }
