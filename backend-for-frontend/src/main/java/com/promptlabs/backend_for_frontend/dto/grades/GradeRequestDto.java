c6000686b3e15772e44abf49a81a857b1a370be8
Lucciano <zorrapeluahihu@gmail.com>
2026-06-30 19:37:50 -0400

    feat: Agrega ms-notas a bff

---
+++ backend-for-frontend/src/main/java/com/promptlabs/backend_for_frontend/dto/grades/GradeRequestDto.java
@@ -0,0 +1,23 @@
+package com.promptlabs.backend_for_frontend.dto.grades;
+
+import jakarta.validation.constraints.*;
+import java.util.UUID;
+
+public record GradeRequestDto(
+
+        @DecimalMax(message = "la nota maxina es 7.0", value = "7.0")
+        @DecimalMin(message = "la nota minima es 1.0", value = "1.0")
+        double value,
+
+        @NotNull
+        UUID studentId,
+
+        @NotNull
+        UUID teacherId,
+
+        @Size(min = 3, max = 70)
+        @NotNull
+        @NotBlank
+        String name
+) {
+}
